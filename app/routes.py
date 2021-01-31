eab415f4ecb0ba5e44e575eb6d0b408d19bb54a4
Kishan <kishanvekaria123@gmail.com>
2021-01-31 21:25:46 +0000

    second commit. Htmls and routes added

--- app/routes.py
+++ app/routes.py
@@ -2,5 +2,14 @@ from flask import render_template
 from app import app
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template("home.html")
+
+@app.route('/pokemondatabase')
+def pokemondatabase():
+    return render_template("pokemondatabase.html")
+
+@app.route('/catchpokemon')
+def catchpokemon():
+    return render_template("catchpokemon.html")
