efee532cf805e0851b3b80319973746ad8a562b4
Kishan <kishanvekaria123@gmail.com>
2021-01-31 23:28:43 +0000

    third commit data tables sql added

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from wtforms import StringField, SubmitField, IntergerField
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'SECRET_KEY'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 
 db = SQLAlchemy(app)
+
+class Pokedata(db.Model):
+    id = db.Column(db.Integer, primary_key =True)
+    poke_id = db.Column(db.Integer)
+    name = db.Column(db.String(30))
+    poketype = db.Column(db.String(30))
+
+    def __init__(self,poke_id,name,poketype):
+        self.poke_id = poke_id
+        self.name =  name
+        self.poketype = poketype
 
 from app import routes, models
